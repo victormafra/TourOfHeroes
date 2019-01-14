c33e69dae95dc99081a0c38e65ec56a39abc350a
Victor Mafra <vlmmafra@gmail.com>
2019-01-14 14:25:25 -0200

    Added an option to delete a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -59,6 +59,20 @@ export class HeroService {
     );
   }
 
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+ 
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
