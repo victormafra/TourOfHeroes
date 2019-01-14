a30e64a578f348f7d83cc6e4bce8dabe3af7fff4
Victor Mafra <vlmmafra@gmail.com>
2019-01-14 11:57:43 -0200

    Added event click handler and selected class when a hero is selected

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,6 +11,11 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit() {
