08fd2a1bf5fa982b1c78579fb88418280f5aa9a2
Victor Mafra <vlmmafra@gmail.com>
2019-01-14 14:32:02 -0200

    Created the hero search component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -66,10 +66,20 @@ export class HeroService {
 
     const id = typeof hero === 'number' ? hero : hero.id;
     const url = `${this.heroesUrl}/${id}`;
- 
+
     return this.http.delete<Hero>(url, httpOptions).pipe(
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
 
